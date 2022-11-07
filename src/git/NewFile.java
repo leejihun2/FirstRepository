12373852665515134232ac4985c537208f87c4cd
leejohun <yi10286@gmail.com>
2022-11-07 14:39:40 +0900

    두번째 작업 완료

--- src/git/NewFile.java
+++ src/git/NewFile.java
@@ -3,9 +3,9 @@ package git;
 public class NewFile {
 
 	public static void main(String[] args) {
-		System.out.println("두번째파일");
 		
+		System.out.println("두번째파일");
+		System.out.println("이것도 두번째 작업 끝");
 	}
 	
-
 }
